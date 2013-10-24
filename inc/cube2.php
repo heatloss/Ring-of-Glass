91e35c062463d2c02f0c37e5c939ddb179128782
Mike Stamm <michael.stamm@wsj.com>
2013-10-24 15:10:11 -0400

    Adding a map view, although it doesn't look like much without 3d elevation.

--- inc/cube2.php
+++ inc/cube2.php
@@ -3,19 +3,16 @@
 		<div id="cube">
 			<div class="rotator">
 				<div class="face precinct">
-					<div class="scenePanel frank" data-name="frank">
-					</div>
-					<div class="scenePanel desktop" data-name="desktop">
-					</div>
+					<div class="scenePanel frank" data-name="frank"></div>
+					<div class="scenePanel desktop" data-name="desktop"></div>
 				</div>
 				<div class="face overlay zoom"></div>
+				<div class="face overlay map"></div>
 			</div>
 			<div class="rotator">
 				<div class="face precinct">
-					<div class="scenePanel printer" data-name="printer">
-					</div>
-					<div class="scenePanel phone" data-name="phone">
-					</div>
+					<div class="scenePanel printer" data-name="printer"></div>
+					<div class="scenePanel phone" data-name="phone"></div>
 					<!-- 
 <div id="todo" class="todoPanel" data-name="checkout">
 					<div class="tilter">
@@ -27,32 +24,33 @@
  -->
 				</div>
 				<div class="face overlay zoom"></div>
+				<div class="face overlay map"></div>
 			</div>
 			<div class="rotator">
 				<div class="face precinct">
-					<div class="scenePanel readout" data-name="readout">
-					</div>
+					<div class="scenePanel readout" data-name="readout"></div>
 <!-- 
 					<div class="todoPanel" data-name="getpassword">
 					</div>
  -->
 				</div>
 				<div class="face overlay zoom"></div>
+				<div class="face overlay map"></div>
 			</div>
 			<div class="rotator">
-				<div class="face precinct">
-				</div>
+				<div class="face precinct"></div>
 				<div class="face overlay zoom"></div>
+				<div class="face overlay map"></div>
 			</div>
 			<div id="top">
-				<div class="face precinct">
-				</div>
+				<div class="face precinct"></div>
 				<div class="face overlay zoom"></div>
+				<div class="face overlay map"></div>
 			</div>
 			<div id="bottom">
-				<div class="face precinct">
-				</div>
+				<div class="face precinct"></div>
 				<div class="face overlay zoom"></div>
+				<div class="face overlay map"></div>
 			</div>
 		</div>
 	</div>
